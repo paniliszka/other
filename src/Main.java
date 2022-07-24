f6bafa1f710ac70c24c96f0e62a2dfe109975f5d
Paulina <pau.jastrzab@gmal.com>
2022-07-24 12:09:32 +0200

    switch2

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,35 @@ public class Main {
          audi.setAutomaticGearbox(true);
          System.out.println(audi.getAutomaticGearbox());
 
-        Kalkulator calc = new Kalkulator();
-        calc.add(5, 5);
-        System.out.println(calc.add(5, 5));*/
+         Kalkulator calc = new Kalkulator();
+         calc.add(5, 5);
+         System.out.println(calc.add(5, 5));*/
 
         Kalkulator kalkulator = new Kalkulator();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Provide the first number");
         int firstval = scanner.nextInt();
-        System.out.println("Tell me what you would like to do. Type: 1 to add, 2 to substract, 3 to multiply, 4 to divide");
-        String val = scanner.next();
         System.out.println("Provide the second number");
         int secondval = scanner.nextInt();
+        System.out.println("Tell me what you would like to do. Type: 1 to add, 2 to substract, 3 to multiply, 4 to divide");
+        String val = scanner.next();
 
 
-        switch (val){
+        switch (val) {
             case "1":
-                System.out.println(kalkulator.add(firstval,secondval));
-            break;
+                System.out.println(kalkulator.add(firstval, secondval));
+                break;
             case "2":
-                System.out.println(kalkulator.sub(firstval,secondval));
-            break;
+                System.out.println(kalkulator.sub(firstval, secondval));
+                break;
             case "3":
-                System.out.println(kalkulator.mult(firstval,secondval));
-            break;
+                System.out.println(kalkulator.mult(firstval, secondval));
+                break;
             case "4":
-                System.out.println(kalkulator.div(firstval,secondval));
+                System.out.println(kalkulator.div(firstval, secondval));
                 break;
 
         }
-
 
 
     }
